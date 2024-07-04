b251c967b443f2a0beabc68437a92d762c32c9e9
Turing158 <15818961209@163.com>
2024-07-04 23:02:25 +0800

    组件页面

---
+++ src/views/CodeView.vue
@@ -0,0 +1,94 @@
+<template>
+    <div class="main">
+        <a class="back" href="/">返回</a>
+        <div class="item">
+            <div class="name">默认/primary</div>
+            <div class="comp">
+                <Code/>
+            </div>
+        </div>
+        <div class="item">
+            <div class="name">自定样式/Custom Style</div>
+            <div class="comp">
+                <Code width="100px" height="60px" margin="20px" alt="CCCCCCCCode"/>
+            </div>
+        </div>
+        <div class="item">
+            <div class="name">自定义变量/Custom Prop</div>
+            <div class="comp">
+                <Code prop="abc"/>
+            </div>
+        </div>
+        <div class="item">
+            <div class="name">组件信息/Component information</div>
+            <div class="comp">
+                <Code ref="code"/>
+                <div style="margin: 20px;">
+                    <a @click="refresh">刷新/refresh</a>
+                    <a @click="getCode">获取数值/Get RandomNum</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+import Code from "@/components/Code.vue";
+import { ref } from "vue";
+const code = ref(null)
+const refresh = ()=>{
+    code.value.refresh()
+}
+const getCode = ()=>{
+    console.log("获取数值/Get RandomNum:",code.value.randomNum);
+}
+</script>
+<style scoped>
+@import url('/src/assets/font/main.css');
+  .main{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100vh;
+    background-color: #f1f1f1;
+    font-family: Inter,HarmonyOS Sans SC, sans-serif;
+    font-feature-settings: 'liga' 1, 'calt' 1;
+  }
+  .back{
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  a{
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid #888;
+    border-radius: 5px;
+    text-decoration: none;
+    color: #131313;
+    transition: all .2s;
+    cursor: pointer;
+  }
+  a:hover{
+    background-color: #888;
+    color: #fff;
+  }
+  .item{
+    margin: 20px 0;
+    padding: 10px 20px;
+    border: 1px solid #000;
+    display: inline;
+    border-radius: 10px;
+  }
+  .item .name{
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 10px;
+  }
+  .item .comp{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
---
+++ src/views/CropperView.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="main">
+        <p>
+            剪切需安装 vue-cropper@1.1.1 和 element-plus <br>
+            Cropper needs to install vue-cropper@1.1.1 and element-plus
+        </p>
+        <p>
+            剪切待更新【目前仅支持1：1剪切，以及选择图片大小的宽高，待更新各种样式】<br>
+            Cropper to be updated【Currently only supports 1:1 cropping, as well as selecting the width and height of the image size, to be updated with various styles】
+        </p>
+        <br>
+        <div>
+            <Cropper />
+        </div>
+    </div>
+</template>
+<script setup>
+import Cropper from '@/components/Cropper.vue';
+</script>
+<style scoped>
+.main{
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
+}
+p{
+    text-align: center;
+    font-size: 20px;
+    margin-bottom: 20px;
+}
+</style>
